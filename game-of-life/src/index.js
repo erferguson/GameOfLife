9f4a1cd7a9f39de86f7d46360b318adfccdf7da0
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-09-17 15:35:33 -0700

    basic layout & starting structure setup

--- game-of-life/src/index.js
+++ game-of-life/src/index.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import GameOfLife from './GameOfLife';
+
+import Grid from './components/Grid'
+
+class Main extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      generation: 0,
+    }
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>Game of Life</h1>
+        <Grid 
+        />
+        <h2>Generations: {this.state.generation}</h2>
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <GameOfLife />
+    <Main />
   </React.StrictMode>,
   document.getElementById('root')
 );
