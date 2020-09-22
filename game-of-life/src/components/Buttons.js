0d9387872b0172927ff5cf11833c76c7babcb5e9
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-09-21 18:26:00 -0700

    buttons >> updated styling to nested ButtonGroup w/ Dropdown

--- game-of-life/src/components/Buttons.js
+++ game-of-life/src/components/Buttons.js
@@ -1,48 +1,55 @@
-import React from 'react'
-import '../index.css'
+import React from "react";
+import "../index.css";
 
-import { ButtonToolbar, DropdownButton, Dropdown } from "react-bootstrap";
+import {
+  Button,
+  ButtonGroup,
+  ButtonToolbar,
+  DropdownButton,
+  Dropdown
+} from "react-bootstrap";
 // import { Dropdown } from "react-bootstrap";
 
 export default class Buttons extends React.Component {
+  handleSelect = evt => {
+    this.props.gridSize(evt);
+  };
 
-    handleSelect = (evt) => {
-        this.props.gridSize(evt)
-    }
+  render() {
+    return (
+      <div className="center">
+        <ButtonGroup>
+          <Button className="btn btn-default" onClick={this.props.playButton}>
+            Play
+          </Button>
+          <Button className="btn btn-default" onClick={this.props.pauseButton}>
+            Pause
+          </Button>
+          <Button className="btn btn-default" onClick={this.props.clear}>
+            Clear
+          </Button>
+          <Button className="btn btn-default" onClick={this.props.slow}>
+            Slow
+          </Button>
+          <Button className="btn btn-default" onClick={this.props.fast}>
+            Fast
+          </Button>
+          <Button className="btn btn-default" onClick={this.props.seed}>
+            Seed
+          </Button>
 
-    render(){
-        return (
-            <div className="center">
-              <ButtonToolbar>
-                <button className="btn btn-default" onClick={this.props.playButton}>
-                  Play
-                </button>
-                <button className="btn btn-default" onClick={this.props.pauseButton}>
-                  Pause
-                </button>
-                <button className="btn btn-default" onClick={this.props.clear}>
-                  Clear
-                </button>
-                <button className="btn btn-default" onClick={this.props.slow}>
-                  Slow
-                </button>
-                <button className="btn btn-default" onClick={this.props.fast}>
-                  Fast
-                </button>
-                <button className="btn btn-default" onClick={this.props.seed}>
-                  Seed
-                </button>
-              </ButtonToolbar>
-                <DropdownButton
-                  title="Grid Size"
-                  id="size-menu"
-                  onSelect={this.handleSelect}
-                >
-                    <Dropdown.Item eventKey="1">20x10</Dropdown.Item>
-                    <Dropdown.Item eventKey="2">50x30</Dropdown.Item>
-                    <Dropdown.Item eventKey="3">70x50</Dropdown.Item>
-                </DropdownButton>
-            </div>
-          );
-    } 
+          <DropdownButton
+            as={ButtonGroup}
+            title="Dropdown"
+            id="size-menu"
+            onSelect={this.handleSelect}
+          >
+            <Dropdown.Item eventKey="1">20x10</Dropdown.Item>
+            <Dropdown.Item eventKey="2">50x30</Dropdown.Item>
+            <Dropdown.Item eventKey="3">70x50</Dropdown.Item>
+          </DropdownButton>
+        </ButtonGroup>
+      </div>
+    );
+  }
 }
