608e141a92656b3ee8f59d8fd2161db4a2c86f65
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-09-23 15:40:50 -0700

    About > added bs>Accordion + Card, adjusted basic css to center

--- game-of-life/src/components/About.js
+++ game-of-life/src/components/About.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import {Accordion, Card } from 'react-bootstrap'
+
 
 export default class About extends React.Component {
     render(){
         return (
-            <div></div>
+            <div className='accordionDiv'>
+                <Accordion defaultActiveKey="0" className='accordionMain'>
+                    <Card className='accordionCard'>
+                        <Accordion.Toggle as={Card.Header} eventKey="0">
+                        Rules of the Game
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="0">
+                        <Card.Body>Hello! I'm the body</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                </Accordion>
+            </div>
         )
     }
 }
